1d160137048d948b07943dfd194125dd5b268249
Thennarasu27 <thennarasum2705@gmail.com>
2026-09-23 05:12:44 +0000

    Initial Commit

---
+++ spendgate/spendgate/doctype/department/department.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Thennarasu M and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Department", {
+// 	refresh(frm) {
+
+// 	},
+// });
